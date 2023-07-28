05f67299e8f892b72012848f423b01ce69be26b2
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2023-07-28 14:56:08 -0500

    Create cardScroll.js

---
+++ cardScroll.js
@@ -0,0 +1,24 @@
+function loopNext(){
+    $('#cardContainer').stop().animate({scrollLeft:'+=75'}, 'fast', 'linear', loopNext);
+}
+
+function loopPrev(){
+    $('#cardContainer').stop().animate({scrollLeft:'-=75'}, 'fast', 'linear', loopPrev);
+}
+
+function stop(){
+    $('#cardContainer').stop();
+}
+
+
+$('#next').hover(function () {
+loopNext();
+},function () {
+stop();
+});
+
+$('#prev').hover(function () {
+loopPrev();
+},function () {
+stop();
+});
