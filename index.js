4808115b05fbd4324d18dcd5443d70b576827ee3
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2023-07-28 14:56:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 window.onload = function(){
 
     const cont = document.getElementById('cardContainer');
-    const mImg = document.getElementById('mainImg');
-    const mInfo = document.getElementById('mainInfo');
+    const mImg = document.getElementById('imgId');
+    const title = document.getElementById('mhOne');
+    const rate = document.getElementById('mhTwo');
+    const series = document.getElementById('mRun');
+    const release = document.getElementById('mRelease');
+
     const headers = {
         'x-rapidapi-host': 'imdb8.p.rapidapi.com',
         'x-rapidapi-key': ''
@@ -14,7 +18,6 @@ fetch("https://imdb8.p.rapidapi.com/title/get-top-rated-tv-shows", {
 })
 .then(response => response.json())
 .then ( data =>{
-    console.log(data);
     let text = "";
     let i = 0;
     let y = 0;
@@ -41,59 +44,45 @@ fetch("https://imdb8.p.rapidapi.com/title/get-top-rated-tv-shows", {
         }
     };
 
-    function myFunction(v){
-        console.log(newArray[0]);
+
+    function mainFunction(num){
+
+                
+                mImg.src = newArray[num].title.image.url;
+                mImg.alt = newArray[num].title.title + " Poster";
+
+                title.textContent = newArray[num].title.title;
+
+                rate.textContent = "IMDb Rating: " + newArray[num].ratings.rating;
+                    if(newArray[num].title.seriesEndYear == undefined ){
+                        series.textContent = "Start " + newArray[num].title.seriesStartYear + " - present";
+                    }else{
+                        series.textContent = "Start " + newArray[num].title.seriesStartYear + " - End " + newArray[num].title.seriesEndYear;
+                    };
+        
+                release.textContent = "Release Date: " + newArray[num].releaseDate;
+    }
+  
+ 
+
+    function cardFunction(v){
         for(let s = v; s < newArray.length; s++){
             const div = document.createElement('div');
             const img = document.createElement('img');
-            const title = document.createElement('h1');
-            const rate = document.createElement('h2');
-            const series = document.createElement('p');
-            const release = document.createElement('p');
-    
+            
                 div.classList.add("card");
                 div.appendChild(img);
                 img.src = newArray[s].title.image.url;
                 img.alt = newArray[s].title.title + " Poster";
-                div.appendChild(title);
-                title.textContent = newArray[s].title.title;
-                div.appendChild(rate);
-                rate.textContent = "IMDb Rating: " + newArray[s].ratings.rating;
-                div.appendChild(series);
-                    if(newArray[s].title.seriesEndYear == undefined ){
-                        series.textContent = "Start " + newArray[s].title.seriesStartYear + " - present";
-                    }else{
-                        series.textContent = "Start " + newArray[s].title.seriesStartYear + " - End " + newArray[s].title.seriesEndYear;
-                    };
-                div.appendChild(release);
-                release.textContent = "Release Date: " + newArray[s].releaseDate;
                 cont.appendChild(div);
+
+            img.onclick = function(){
+
+                    mainFunction(s);
+                }
         }
 
-        const img = document.createElement('img');
-        const title = document.createElement('h1');
-        const rate = document.createElement('h2');
-        const series = document.createElement('p');
-        const release = document.createElement('p');
-
-                mImg.appendChild(img);
-                img.src = newArray[0].title.image.url;
-                img.alt = newArray[0].title.title + " Poster";
-
-
-                mInfo.appendChild(title);
-                title.textContent = newArray[0].title.title;
-                mInfo.appendChild(rate);
-                rate.textContent = "IMDb Rating: " + newArray[0].ratings.rating;
-                mInfo.appendChild(series);
-                    if(newArray[0].title.seriesEndYear == undefined ){
-                        series.textContent = "Start " + newArray[0].title.seriesStartYear + " - present";
-                    }else{
-                        series.textContent = "Start " + newArray[0].title.seriesStartYear + " - End " + newArray[0].title.seriesEndYear;
-                    };
-                mInfo.appendChild(release);
-                release.textContent = "Release Date: " + newArray[0].releaseDate;
-        
+            mainFunction(0);
     }
 
     
@@ -112,38 +101,12 @@ fetch("https://imdb8.p.rapidapi.com/title/get-top-rated-tv-shows", {
         z++;
     }
 
-    myFunction(0);
+    cardFunction(0);
 
 });
 })
 .catch(err => {
     console.error(err);
  }); 
-
-
  
-        function loopNext(){
-            $('#cardContainer').stop().animate({scrollLeft:'+=75'}, 'fast', 'linear', loopNext);
-        }
-
-        function loopPrev(){
-            $('#cardContainer').stop().animate({scrollLeft:'-=75'}, 'fast', 'linear', loopPrev);
-        }
-
-        function stop(){
-            $('#cardContainer').stop();
-        }
-
-
-        $('#next').hover(function () {
-        loopNext();
-        },function () {
-        stop();
-        });
-
-        $('#prev').hover(function () {
-        loopPrev();
-        },function () {
-        stop();
-        });
 }
